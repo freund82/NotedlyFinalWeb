702929ec0224714c20a4bbf76cdac8c5a2417381
Maxim Utkin <maxim2001_2001@mail.ru>
2021-12-05 23:06:34 +0300

    NoteFeed BlockStyle

--- src/components/NoteFeed.js
+++ src/components/NoteFeed.js
@@ -8,6 +8,9 @@ margin:0 auto;
 margin-bottom:2em;
 padding-bottom:2em;
 border-bottom:1px solid #F5F4F0;
+`;
+
+const BlockStyle=styled.div`
 overflow:scroll;
 `;
 
@@ -15,14 +18,14 @@ import Note from './Note';
 
 const NoteFeed=({notes})=>{
     return(
-        <div>
+        <BlockStyle>
             {notes.map(note=>(
                 <NoteWrapper key={note.id}>
                     <Note note={note}></Note>
                     <Link to={`note/${note.id}`}>Permalink</Link>
                 </NoteWrapper>
             ))}
-        </div>
+        </BlockStyle>
     );
 };
 
